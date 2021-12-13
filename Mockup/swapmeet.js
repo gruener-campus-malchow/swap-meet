1a47b3f301cc9878785d2e8f11f8e1ebc928ae40
M&M <mika.merten@googlemail.com>
2021-12-13 13:29:25 +0100

    update

--- Mockup/swapmeet.js
+++ Mockup/swapmeet.js
@@ -11,17 +11,16 @@ document.addEventListener('DOMContentLoaded', function ()
 function test()
 {
 
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "/api/item/", true);
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "/api/item", true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.send(JSON.stringify({
     title: 'testtitel',
     description: 'this is a helpful description'
 }));
 xhr.onload = function() {
-  console.log(this.responseText);
-  var data = JSON.parse(this.responseText);
+  console.log(this);
+  let data = JSON.parse(this.responseText);
   console.log(data);
-  return value;
 }
 }
