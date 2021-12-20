2adff497cc720aa43d78a88b501827a0f0e85527
M&M <mika.merten@googlemail.com>
2021-12-20 13:24:27 +0100

    JS

--- Mockup/swapmeet.js
+++ Mockup/swapmeet.js
@@ -6,13 +6,14 @@ document.addEventListener('DOMContentLoaded', function ()
     // weiterleitung auf andere Views
 
     document.getElementById('name').addEventListener('click', test);
+    document.getElementById('name').addEventListener('click', testtest);
 }
 );
 function test()
 {
 
 let xhr = new XMLHttpRequest();
-xhr.open("POST", "/api/category", true);
+xhr.open("GET", "/api/item", true);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.send(JSON.stringify({
     title: 'testtitel',
@@ -23,4 +24,23 @@ xhr.onload = function() {
   let data = JSON.parse(this.responseText);
   console.log(data);
 }
-}
+};
+
+function testtest()
+{
+//setup our table array
+var tableArr = [
+  ["row 1, cell 1", "row 1, cell 2"],
+  ["row 2, cell 1", "row 2, cell 2"]
+]//create a Table Object
+let table = document.createElement('table');//iterate over every array(row) within tableArr
+for (let row of tableArr) {//Insert a new row element into the table element
+  table.insertRow();//Iterate over every index(cell) in each array(row)
+  for (let cell of row) {//While iterating over the index(cell)
+//insert a cell into the table element
+    let newCell = table.rows[table.rows.length - 1].insertCell();//add text to the created cell element
+    newCell.textContent = cell;
+  }
+}//append the compiled table to the DOM
+document.body.appendChild(table);
+};
