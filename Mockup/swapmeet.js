c1b9c301ec76abec4d7463610e9158a9ca229bd6
42Strike <mika.merten@googlemail.com>
2021-12-31 18:44:12 +0100

    zwischenergebnis

--- Mockup/swapmeet.js
+++ Mockup/swapmeet.js
@@ -1,45 +1,45 @@
 'use strict';
 //alert("hilfe");
-document.addEventListener('DOMContentLoaded', function ()
-{ 
+document.addEventListener('DOMContentLoaded', function() {
     // weiterleitung auf andere Views
-    //document.getElementById('name').addEventListener('click', test);
-    document.getElementById('name').addEventListener('click', testtest);
-}
-);
-function test()
-{
+    document.getElementById('name').addEventListener('click', test);
+    //document.getElementById('name').addEventListener('click', testtest);
+});
 
-let xhr = new XMLHttpRequest();
-xhr.open("GET", "/api/item", true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(JSON.stringify({
-    title: 'testtitel',
-    description: 'this is a helpful description'
-}));
-xhr.onload = function() {
-  console.log(this);
-  let data = JSON.parse(this.responseText);
-  console.log(data);
-}
-};
+function test() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/api/category", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        title: 'testtitel',
+        description: 'this is a helpful description'
+    }));
+    xhr.onload = function() {
+        console.log(this);
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+		
+	const elemdiv = document.createElement('div');
+    elemdiv.setAttribute('id', 'test42');
+    elemdiv.style.cssText = 'margin-left: 250px;';
 
-function testtest()
-{
-alert ("hello world");
-//setup our table array
-var tableArr = [
-  ["row 1, cell 1", "row 1, cell 2"],
-  ["row 2, cell 1", "row 2, cell 2"]
-]//create a Table Object
-let table = document.createElement('table');//iterate over every array(row) within tableArr
-for (let row of tableArr) {//Insert a new row element into the table element
-  table.insertRow();//Iterate over every index(cell) in each array(row)
-  for (let cell of row) {//While iterating over the index(cell)
-//insert a cell into the table element
-    let newCell = table.rows[table.rows.length - 1].insertCell();//add text to the created cell element
-    newCell.textContent = cell;
-  }
-}//append the compiled table to the DOM
-document.body.appendChild(table);
+    const body = document.querySelector('body');
+    body.appendChild(elemdiv);
+
+    //var data = ["doge", "cate", "birb", "doggo", "moon moon", "awkward seal"];
+
+    var myTable = "<table class='table'><tr><th class='table-row'> Name </th><th class='table-row'> Anzahl der Inserate </th></tr><tr>";
+    var perrow = 1; // x CELLS per Row
+    data.forEach((value, i) => {
+        myTable += `<td class="table-row-left">${value.title}</td>`;
+		myTable += `<td class="table-row-right">${value.id}</td>`;
+        var next = i + 1;
+        if (next % perrow == 0 && next != data.length) {
+            myTable += "</tr><tr>";
+        }
+    });
+    myTable += "</tr></table>";
+    document.getElementById("test42").innerHTML = myTable;
+    }
+
 };
