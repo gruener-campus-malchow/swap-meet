4e3811539f43cb59d5280015b8ec0a222135d4c7
42Strike <mika.merten@googlemail.com>
2021-12-31 22:11:59 +0100

    category List working(partially)

--- Mockup/swapmeet.js
+++ Mockup/swapmeet.js
@@ -1,12 +1,9 @@
 'use strict';
-//alert("hilfe");
 document.addEventListener('DOMContentLoaded', function() {
-    // weiterleitung auf andere Views
-    document.getElementById('name').addEventListener('click', test);
-    //document.getElementById('name').addEventListener('click', testtest);
+    document.getElementById('name').addEventListener('click', categoryTable);
 });
 
-function test() {
+function categoryTable() {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "/api/category", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -18,28 +15,31 @@ function test() {
         console.log(this);
         var data = JSON.parse(this.responseText);
         console.log(data);
-		
-	const elemdiv = document.createElement('div');
-    elemdiv.setAttribute('id', 'test42');
-    elemdiv.style.cssText = 'margin-left: 250px;';
 
-    const body = document.querySelector('body');
-    body.appendChild(elemdiv);
+        const elemdiv = document.createElement('div');
+        elemdiv.setAttribute('id', 'test42');
+        elemdiv.style.cssText = 'margin-left: 250px;';
 
-    //var data = ["doge", "cate", "birb", "doggo", "moon moon", "awkward seal"];
+        const addd = document.querySelector('#test5');
+        addd.appendChild(elemdiv);
 
-    var myTable = "<table class='table'><tr><th class='table-row'> Name </th><th class='table-row'> Anzahl der Inserate </th></tr><tr>";
-    var perrow = 1; // x CELLS per Row
-    data.forEach((value, i) => {
-        myTable += `<td class="table-row-left">${value.title}</td>`;
-		myTable += `<td class="table-row-right">${value.id}</td>`;
-        var next = i + 1;
-        if (next % perrow == 0 && next != data.length) {
-            myTable += "</tr><tr>";
-        }
-    });
-    myTable += "</tr></table>";
-    document.getElementById("test42").innerHTML = myTable;
+        var myTable = "<table id='categoryTable' class='w3-table w3-striped w3-bordered'><tr><th class='table-row'> Name </th><th class='table-row'> Anzahl der Inserate </th></tr><tr>";
+        var perrow = 1;
+        data.forEach((value, i) => {
+            myTable += `<td class="w3-hover-blue" id="${value.id}">${value.title}</td>`;
+            myTable += `<td class="w3-hover-blue">${value.id}</td>`;
+            var next = i + 1;
+            if (next % perrow == 0 && next != data.length) {
+                myTable += "</tr><tr>";
+            }
+        });
+        myTable += "</tr></table>";
+        document.getElementById("test42").innerHTML = myTable;
     }
 
 };
+
+// model category has items needed for row "Anzahl der Inserate
+// model messages not working(no access to data)
+// somehow get id from row that was clicked on for Inserate list
+// event load not working, "onload = ..." in Mockup_Kategorien
