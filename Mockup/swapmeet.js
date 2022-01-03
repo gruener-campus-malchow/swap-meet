7fbcc36ae644c004c509b8e35b4c578936993524
M&M <mika.merten@googlemail.com>
2022-01-03 12:53:39 +0100

    zwischenstand

--- Mockup/swapmeet.js
+++ Mockup/swapmeet.js
@@ -1,15 +1,15 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('name').addEventListener('click', categoryTable);
+    //document.getElementById('body').addEventListener('load', categoryTable);
 });
 
 function categoryTable() {
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", "/api/category", true);
+    xhr.open("GET", "/api/category/", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
-        title: 'testtitel',
-        description: 'this is a helpful description'
+        title: 'dasdwada',
+        description: 'category title'
     }));
     xhr.onload = function() {
         console.log(this);
@@ -26,8 +26,8 @@ function categoryTable() {
         var myTable = "<table id='categoryTable' class='w3-table w3-striped w3-bordered'><tr><th class='table-row'> Name </th><th class='table-row'> Anzahl der Inserate </th></tr><tr>";
         var perrow = 1;
         data.forEach((value, i) => {
-            myTable += `<td class="w3-hover-blue" id="${value.id}">${value.title}</td>`;
-            myTable += `<td class="w3-hover-blue">${value.id}</td>`;
+            myTable += `<td class="w3-hover-blue" id="${value.id}" onClick="Itemlist(this.id)">${value.title}</td>`;
+            myTable += `<td class="w3-hover-green" id="${value.id}" onClick="Itemlist(this.id)">${value.id}</td>`;
             var next = i + 1;
             if (next % perrow == 0 && next != data.length) {
                 myTable += "</tr><tr>";
@@ -39,7 +39,43 @@ function categoryTable() {
 
 };
 
-// model category has items needed for row "Anzahl der Inserate
+function Itemlist(clicked_id) {
+let xhr = new XMLHttpRequest();
+    xhr.open("GET", `/api/category_has_item/category_id/${clicked_id}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        title: 'daswadawdasdwa',
+        description: 'wdadwadawdasdwadsa title'
+    }));
+    xhr.onload = function() {
+        console.log(this);
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+
+        const elemdiv = document.createElement('div');
+        elemdiv.setAttribute('id', 'test42');
+        elemdiv.style.cssText = 'margin-left: 250px;';
+
+        const addd = document.querySelector('#test5');
+        addd.appendChild(elemdiv);
+
+        var myTable = "<table id='categoryTable' class='w3-table w3-striped w3-bordered'><tr><th class='table-row'> Name </th><th class='table-row'> Anzahl der Inserate </th></tr><tr>";
+        var perrow = 1;
+        data.forEach((value, i) => {
+            myTable += `<td class="w3-hover-blue" id="${value.id}" onClick="Itemlist(this.id)">${value.title}</td>`;
+            myTable += `<td class="w3-hover-green" id="${value.id}" onClick="Itemlist(this.id)">${value.id}</td>`;
+            var next = i + 1;
+            if (next % perrow == 0 && next != data.length) {
+                myTable += "</tr><tr>";
+            }
+        });
+        myTable += "</tr></table>";
+        document.getElementById("test42").innerHTML = myTable;
+    }
+
+};
+
+// model category has items needed for column "Anzahl der Inserate"
 // model messages not working(no access to data)
 // somehow get id from row that was clicked on for Inserate list
 // event load not working, "onload = ..." in Mockup_Kategorien
